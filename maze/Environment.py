5ca41d83a979e6b32337ca74d25606693b65688f
leejungi <leejungi@naver.com>
2021-01-21 22:12:56 +0900

    Add maze environment

---
+++ maze/Environment.py
@@ -0,0 +1,79 @@
+import numpy as np
+import random
+
+class Maze:
+	def __init__(self, height=10, width=10):
+		self.height = height
+		self.width = width
+		self.maze = np.zeros((self.height,self.width))
+		self.wall = [[random.randrange(0,self.height), random.randrange(0,self.width)] for i in range(min(self.height,self.width))]
+		self.start = [0,0]
+		self.end = [self.height-1,self.width-1]
+		self.state = self.start
+
+		for i in range(min(self.height,self.width)):
+			self.maze[self.wall[i][0]][self.wall[i][1]]= 1 # wall: 1
+
+		self.maze[self.start[0]][self.start[1]] = 2 # start point
+		self.maze[self.end[0]][self.end[0]] = 3 #Goal
+
+	def get_state(self):
+		return self.state
+
+	def get_hw(self):
+		return self.height, self.width
+
+	def reset(self):
+		self.state = self.start
+
+	def print_maze(self):
+		for h in range(self.height):
+			print(self.maze[self.height-h-1])
+
+	def step(self, action):
+		x= [0,0,-1,1]
+		y= [1,-1,0,0]
+
+		next_state = [y[action], x[action]]
+		next_state[0] += self.state[0]
+		next_state[1] += self.state[1]
+
+		if next_state[0] < 0 or next_state[1] <0 or next_state[0] >=self.height or next_state[1] >= self.width or self.maze[next_state[0]][next_state[1]] == 1:
+			reward = -100
+			done = True
+		elif self.maze[next_state[0]][next_state[1]] == 3:
+			reward = 100
+			done = True
+		else:
+			reward = 0
+			done = False
+
+		self.state = next_state
+
+		return next_state, reward, done
+
+
+	def load(self,file_name):
+		f = open(file_name,"r")
+
+		lines = f.readlines()
+
+		self.maze =[]
+
+		for n, line in enumerate(reversed(lines)):
+			w=[]
+			for i in range(len(line)-1):
+				if int(line[i]) == 2:
+					self.start= [n,i]
+				elif int(line[i]) == 3:
+					self.end = [n,i]
+
+				w.append(int(line[i]))
+
+			self.maze.append(w)
+		
+		self.state = self.start
+		self.height, self.width = np.shape(self.maze)
+		
+			
+
---
+++ maze/SARSA.py
@@ -0,0 +1,101 @@
+from Environment import *
+import random
+import matplotlib.pyplot as plt
+
+class SARSA_policy:
+	def __init__(self, height =10, width =10):
+		self.num_action = 4
+		self.height = height
+		self.width = width
+		self.q_table = np.zeros((self.height,self.width,self.num_action))
+		self.learning_rate = 0.01
+		self.discount_factor = 0.9
+
+	def learn(self,state, action, reward, next_state, next_action):
+
+		q = self.q_table[state[0]][state[1]][action]
+
+		if next_state[0] < 0 or next_state[1] <0 or next_state[0] >=self.height or next_state[1] >= self.width:
+			td_target = reward
+		else:
+			td_target = reward + self.discount_factor * self.q_table[next_state[0]][next_state[1]][next_action]
+
+		self.q_table[state[0]][state[1]][action] += self.learning_rate * (td_target - q)
+
+	def get_action(self, state, epsilon):
+		if state[0] < 0 or state[1] <0 or state[0] >=self.height or state[1] >= self.width:
+			return 0
+
+		if np.random.rand() < epsilon:
+			action = np.argmax(self.q_table[state[0]][state[1]])
+		else:
+			action = np.random.choice([0,1,2,3])
+
+		return action
+
+
+
+if __name__ =="__main__":
+
+	maze = Maze(height=10, width=10)
+	maze.load("maze.txt")
+	height, width = maze.get_hw()
+	policy = SARSA_policy(height, width)
+
+	num_episode = 50000
+	max_step = 10000
+	epsilon = 0.7
+
+	G_list =[0 for i in range(num_episode)]
+
+	#Train
+	for i in range(num_episode):
+		maze.reset()
+
+		right_list=[]
+		for j in range(max_step):
+			state = maze.get_state()
+
+			action = policy.get_action(state, epsilon)
+			right_list.append(action)
+
+			next_state, reward, done = maze.step(action)
+
+			next_action = policy.get_action(next_state, epsilon)
+
+			policy.learn(state, action, reward, next_state, next_action)
+			
+			G_list[i] +=reward
+
+			if done == True:
+				break
+
+	#Test
+	maze.reset()
+	Total_reward =0
+	action_list=[]
+
+	print("Test Start")
+	for j in range(height*width):
+		state = maze.get_state()
+
+		action = policy.get_action(state,1)
+		action_list.append(action)
+
+		next_state, reward, done = maze.step(action)
+
+		next_action = policy.get_action(next_state, 1)
+
+		policy.learn(state, action, reward, next_state, next_action)
+		
+		Total_reward +=reward
+
+		if done == True:
+			break
+
+	maze.print_maze()
+	print(action_list)
+	print("Reward: ", Total_reward)
+
+	plt.plot(G_list,'o')
+	plt.show()
